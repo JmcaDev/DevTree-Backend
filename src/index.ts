ca5125a1727986edee5caeaa6229c1555a50734b
JmcaDev <jmcastilloabreu@gmail.com>
2026-07-15 12:44:02 -0400

    Added Modularization

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import server from "./server.js"
+import colors from 'colors'
+import server from './server.js'
 
 const port = process.env.PORT || 4000
 
 server.listen(port, () => {
-  console.log(`Servidor funcionando en puerto: ${port}`)
+  console.log(colors.green(`Servidor funcionando en puerto: ${port}`))
 })
